bcabb4d776ab414511768ccd92ea58b8db4510a7
dagangyang <hanlinyang@sharkshopping.com>
2023-12-01 14:07:23 +0800

    v12.1 init

---
+++ pages/task/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <view>123</view>
+</template>
---
+++ pages_user/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <view class='content'>
+        <!-- <image src='/static/user/login_bg.webp'/> -->
+    </view>
+    </template>
+
+    <style scoped lang='less'>
+.content {
+    width: 750rpx;
+    min-height: 100vh;
+    background: url('/static/user/login_bg.webp') no-repeat;
+    background-size: 100%;
+    image {
+        width: 100%;
+    }
+}
+</style>
